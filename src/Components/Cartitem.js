776ddef523872a6fab18a5949fe97dbb51ea9ee5
niteshsingh29 <niteshsingh29072000a@gmail.com>
2022-03-14 15:20:19 +0530

    added increase quantity Function

--- src/Components/Cartitem.js
+++ src/Components/Cartitem.js
@@ -1,36 +1,64 @@
-import React from 'react';
+import React from "react";
 
 class Cartitem extends React.Component {
-    render () {
-        return (
-            <div className="cart-item">
-                <div className="left-block">
-                    <img style = {styles.image} alt="not loaded"/>
-                </div>
-                <div className="right-block">
-                    <div style = {{color: '#111'}}>Phone</div>
-                    <div style = {styles.font}>Rs 999</div>
-                    <div style = {styles.font}>Qty: 1</div>
-                    <div className="cart-item-actions">
-                        { }
-                    </div> 
-                </div>
+  constructor() {
+    super();
 
-            </div>
-        )
-    }
+    this.state = {
+      price: 999,
+      qty: 1,
+      title: "Mobile - Phone",
+      img: "",
+    };
+  }
 
+  handleQuantity() {
+      console.log("Quantity added", this.state)
+  }
+  render() {
+    const { title, price, qty } = this.state;
+    return (
+      <div className="cart-item">
+        <div className="left-block">
+          <img style={styles.image} alt="not loaded" />
+        </div>
+        <div className="right-block">
+          <div style={{ color: "#111", fontSize: 45 }}>{title}</div>
+          <div style={styles.font}>Rs {price}</div>
+          <div style={styles.font}>Qty: {qty}</div>
+          <div className="cart-item-actions">
+            {}
+            <img
+              alt="increase"
+              className="action-icons"
+              src="https://as2.ftcdn.net/v2/jpg/00/70/16/29/1000_F_70162903_5mFpUbO3ZfRyD4gslH8j2c5VxjGMKU9G.jpg"
+            onClick = {this.handleQuantity.bind(this)}/>
+            <img
+              alt="decrease"
+              className="action-icons"
+              src="https://as1.ftcdn.net/v2/jpg/03/73/49/86/1000_F_373498649_nBxauQ0ipBSVrVcMpWWVmTpXu3BLvRyY.jpg"
+            />
+            <img
+              alt="delete"
+              className="action-icons"
+              src="https://as2.ftcdn.net/v2/jpg/01/90/89/15/1000_F_190891550_N7uKp2aHE3mOc20dmtDytj7atgvbhdOu.jpg"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 const styles = {
-    image : {
-        height : 110,
-        width : 110,
-        borderRadius : 4,
-        backgroundColor : '#ccc'
-    },
-    font: {
-        fontSize : 25,
-        color : '#777'
-    }
-}
-export default Cartitem
+  image: {
+    height: 110,
+    width: 110,
+    borderRadius: 4,
+    backgroundColor: "#ccc",
+  },
+  font: {
+    fontSize: 25,
+    color: "#777",
+  },
+};
+export default Cartitem;
