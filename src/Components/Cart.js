5e457852375042dd67d82c152fb18bae44bfffd1
niteshsingh29 <niteshsingh29072000a@gmail.com>
2022-03-15 10:55:05 +0530

    add unique id to every object

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -12,36 +12,42 @@ class Cart extends React.Component {
           qty: 1,
           title: "Mobile - Phone",
           img: "",
+          id:1
         },
         {
           price: 1999,
           qty: 1,
           title: "Watch",
           img: "",
+          id:2
         },
         {
           price: 12999,
           qty: 1,
           title: "Laptop",
           img: "",
+          id:3
         },
         {
             price: 500,
             qty: 1,
             title: "Iphone",
             img: "",
+            id:4
           },
           {
             price: 21999,
             qty: 1,
             title: "Macbook",
             img: "",
+            id:5
           },
           {
             price: 5999,
             qty: 1,
             title: "Washing machine",
             img: "",
+            id:6
           }
       ],
     };
@@ -52,7 +58,7 @@ class Cart extends React.Component {
       <div className="cart">
         <h1> Welcome to Prolance Cart</h1>
         {products.map((product) => {
-          return <Cartitem product={product}/>;
+          return <Cartitem product={product} key={product.id}/>;
         })}
       </div>
     );
