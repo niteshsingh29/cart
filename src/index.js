9d08765b96c811c2ac2996c3443e56b7f3592ac3
niteshsingh29 <niteshsingh29072000a@gmail.com>
2022-03-16 18:25:36 +0530

    firebase setup in cart

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBm8aZZR9BNcgjQfGSBHFFY61uTaYy91xE",
+  authDomain: "cart-17b43.firebaseapp.com",
+  projectId: "cart-17b43",
+  storageBucket: "cart-17b43.appspot.com",
+  messagingSenderId: "876260222512",
+  appId: "1:876260222512:web:33e3eb3f8af7ad77e3eff5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
